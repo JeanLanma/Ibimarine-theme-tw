ada22da15edbd61ec09db26a37713765d45d90ca
Jean Langarica <jeanmacario048@gmail.com>
2022-04-09 01:53:47 -0500

    Added Home Page Mobile

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+      './*.html'
+  ],
+  content: [],
+  theme: {
+    colors: {
+        'old-gold': '#AAA173',
+        'old-black': '#3C4045',
+        'white': '#ffffff'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
